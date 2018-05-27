11ecf773893464322f787a82a51fcbe8f98fcd60
Olivier Winsemius <o.winsemius@gmail.com>
2018-05-27 13:15:28 +0200

    initial commit.

---
+++ src/js/redux/reducers/calendar.js
@@ -0,0 +1,13 @@
+const initialState = {
+    loading: false,
+    errors: null,
+    loaded: true,
+    weeks: [],
+};
+
+export default (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
--- src/js/redux/reducers/index.js
+++ src/js/redux/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
+import calendar from './calendar';
 
-export default () => {}; //combineReducers({});
+export default combineReducers({
+    calendar,
+});
